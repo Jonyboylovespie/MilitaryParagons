c9c31d99c9722d1bb4815a1cb8ff5e915d67645e
Jonyboylovespie <90563990+Jonyboylovespie@users.noreply.github.com>
2024-02-02 23:33:43 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,7 +6,7 @@
         public const string Version = "1.0.7";
         public const string Name = "Military Paragons";
         public const string Description =
-            "This mod adds paragons for all of the military towers.<br><br>This is originally Greenphx's mod, I have just fixed it to work in the newer BTD6 versions and added new features.";
+            "This mod adds paragons for all of the military towers. This is originally Greenphx's mod, I have just fixed it to work in the newer BTD6 versions.";
 
         public const string RepoOwner = "Jonyboylovespie";
         public const string RepoName = "MilitaryParagons";
